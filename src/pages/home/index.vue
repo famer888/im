429e63acced70573d519b8bacec19b2a5f42e0d8
jon <web-jon>
2025-08-22 11:59:10 +0800

    版本号修改为1.6.5

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -470,7 +470,7 @@ export default {
           const today = dayjs().format("YYYY-MM-DD");
 
           if (
-            (res.version > Number("1.6.6".replace(/\./g, "")) &&
+            (res.version > Number("1.6.5".replace(/\./g, "")) &&
               deviceConfig.upVersionDay !== today) ||
             res.flag === 2
           ) {
