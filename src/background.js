8166013ff2cad019f647259351f58b7e15f56e7f
jon <web-jon>
2025-09-24 17:49:08 +0800

    版本号刚刚为1.6.6

--- src/background.js
+++ src/background.js
@@ -273,7 +273,7 @@ function updateTray(unread = 0) {
                     updateTray(0);
                     mainWindow.show();
                 });
-                tray.setToolTip("【ocs 版本1.6.5】");
+                tray.setToolTip("【ocs 版本1.6.6】");
             }
 
             if (isOsx) {
--- src/pages/home/home-left/send-helper.vue
+++ src/pages/home/home-left/send-helper.vue
@@ -23,7 +23,7 @@ export default {
   data() {
     return {
       pageName: '版本信息',
-      version: '1.6.5'
+      version: '1.6.6'
     }
   },
   methods: {
