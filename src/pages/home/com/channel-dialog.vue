da0e61c9c9bd60200660e2397a83ec60119b1976
web-jon <965224493@qq.com>
2025-11-03 18:48:48 +0800

    频道弹窗已加入隐藏加入按钮

--- src/pages/home/com/channel-dialog.vue
+++ src/pages/home/com/channel-dialog.vue
@@ -11,7 +11,7 @@
         />
       <span>{{ info.channelName }}</span>
       <p class="text-clamp-2">{{ info.remark }}</p>
-      <button  @click="handleJoinChannel">
+      <button  @click="handleJoinChannel" v-if="!info.memberType">
         加入频道
       </button>
     </div>
