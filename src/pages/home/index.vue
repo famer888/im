4ac9e87b2048994d29fb308630f8224d384101e7
six <six@test.com>
2025-11-07 17:56:56 +0800

    fix: 切换到频道infoactive为空

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -302,12 +302,16 @@ export default {
                   operator: "channelDetailCache",
                   data: channelDetail,
             });
-            this.infoActive = {
-              ...info,
-              ...channelDetail,
-              isDisable: channelDetail.status === 3,
-              channelDetailDone: +new Date(),
-            };
+            // 快速切换时接口未返回，就别赋值了，否则会覆盖掉当前的值
+            if ((this.infoActive?.channelId || this.infoActive?.id) === channelDetail?.channelId) {
+              this.infoActive = {
+                ...info,
+                ...channelDetail,
+                isDisable: channelDetail.status === 3,
+                channelDetailDone: +new Date(),
+              };
+            }
+
           });
         } else if (info) {
           curGroup = this.groupList.find((item) => item.id == info.id);
