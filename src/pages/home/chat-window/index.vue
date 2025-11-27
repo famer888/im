ba53f57567b418a2883f4f781f746eda549357b8
six <six@test.com>
2025-11-27 12:12:36 +0800

    fix: 侧边栏视窗抖动

--- src/pages/home/chat-window/index.vue
+++ src/pages/home/chat-window/index.vue
@@ -1286,6 +1286,9 @@ export default {
   position: relative;
   display: flex;
   flex-direction: column;
+  padding-right: 0;
+  transition: padding-right 0.3s ease;
+  will-change: padding-right;
   &.sidebar-outer {
     padding-right: 256px;
   }
