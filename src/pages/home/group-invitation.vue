a82799c882bec8a69a86b0ef2134272104e03ec5
web-my <qweegaadssvdsgdsf@proton.me>
2025-12-17 13:43:27 +0800

    fix: 添加调试日志以检查获取群组请求列表的响应
    添加console.log以调试getGroupReqList接口返回的数据，便于排查群组请求列表显示问题

--- src/pages/home/group-invitation.vue
+++ src/pages/home/group-invitation.vue
@@ -113,6 +113,7 @@ export default {
 
     handleUpdateList() {
       getGroupReqList({ pageNum: this.pageNum, pageSize: 100 }).then((res) => {
+        console.log('getGroupReqList--',res)
         if (res && res.groupReqs) {
           let groupInfoList = [];
           res.groupReqs.forEach((item) => {
