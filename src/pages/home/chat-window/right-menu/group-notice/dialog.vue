b5b8401a49aeb2ba777b88c3b8362b7edce96edb
web-my <qweegaadssvdsgdsf@proton.me>
2025-12-22 16:48:36 +0800

    fix(群公告): 添加公告内容为空时的校验
    当用户未输入公告内容时，显示提示信息并阻止提交

--- src/pages/home/chat-window/right-menu/group-notice/dialog.vue
+++ src/pages/home/chat-window/right-menu/group-notice/dialog.vue
@@ -200,6 +200,10 @@ export default {
      * 提交
      */
     handleOk() {
+      if(!this.noticeText){
+        window.$toast(this.$t("请输入内容"));
+        return;
+      }
       if (this.bfAll) {
         window
           .$confirm({
